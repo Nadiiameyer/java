d366c82a382fdcbbe4d1cabcb6e42ae170a196fb
Nadiiameyer <113910299+Nadiiameyer@users.noreply.github.com>
2022-10-19 09:23:08 +0200

    Add files via upload

---
+++ Homework3.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("write a word with 4 letters");
+        // String string = s.nextLine();
+        // System.out.println("you wrote a word with 4 letters");
+        ////////////////////////////////
+        // int a=20, b=15;
+        //int c=a+b;
+        // System.out.println(c);
+
+        //float a = 5.8f;
+        // float      b = 7.6f;
+        //double c = 5/a;
+        //System.out.println(c);
+        // int choice;
+        //  double amount;
+        //     double dollar, euro;
+
+        //   System.out.println("Enter the number");
+        //  Scanner s = new Scanner(System.in);
+
+        //  System.out.println("Options");
+        // System.out.println("Enter 1: Dollar");
+        // System.out.println("Enter 2: Euro");
+        //System.out.println("Choose your option: ");
+
+
+        //choice = s.nextInt();
+        // System.out.println("Your number is: " + choice);
+        //System.out.println("Enter the amount you want to convert");
+        // amount = s.nextFloat();
+        // System.out.println("Your amount is " + amount);
+
+//euro = amount * 1.03;
+        // System.out.println(amount + "Euro=" + euro + "Euro");
+//dollar =amount * 1;
+        //  System.out.println(amount + "Dollar" + euro + "Euro");
+Scanner canWalk = new Scanner(System.in);
+
+boolean isWeekend = true;
+boolean isRain = false;
+isWeekend = canWalk.nextBoolean();
+
+if (isWeekend == true)
+    System.out.println("we canWalk");
+else {
+    System.out.println("we can'tWalk");
+}
+if (isRain == false);
+else {
+    System.out.println("we canWalk");
+}
+
+
+
+
+    }
+    }
